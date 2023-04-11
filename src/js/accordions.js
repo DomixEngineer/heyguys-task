482d6a4b56b792b23d3cb7e55c5b59d162fc6234
DominikIT <Dominik.Dziewulski@wp.pl>
2023-04-11 16:43:47 +0200

    Accordions improvment

--- src/js/accordions.js
+++ src/js/accordions.js
@@ -1,10 +1,23 @@
-const accordions = document.getElementsByClassName('single-accordion');
+const accordions = document.getElementsByClassName('single-accordion')
+const inactiveClassName = 'single-accordion__body--inactive'
+const openAccordionImgSrc = './src/svg/accordion_open_btn.png'
+const closeAccordionImgSrc = './src/svg/accordion_exit_btn.png'
+
 if (accordions && accordions.length > 0) {
     for (let x = 0; x < accordions.length; x++) {
         let singleAccordion = accordions[x]
         let singleAccordionIcon = singleAccordion.children[0].children[0]
-        singleAccordionIcon.addEventListener('click', (el) => {
-            console.log(el.parentElement)
+        let singleAccordionIconImg = singleAccordion.children[0].children[0].children[0]
+        singleAccordionIcon.addEventListener('click', (event) => {
+            let body = event.target.parentNode.parentNode.nextElementSibling
+            let isInactive = body.classList.contains(inactiveClassName)
+            if (isInactive) {
+                body.classList.remove(inactiveClassName)
+                singleAccordionIconImg.src = closeAccordionImgSrc;
+            } else {
+                body.classList.add(inactiveClassName)
+                singleAccordionIconImg.src = openAccordionImgSrc;
+            }
         })
     }
 } 
